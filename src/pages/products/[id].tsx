6276f5ad0aa1161f22731f4bd8e388c1db9d446b
kykim00 <jeongsaeki@likelion.org>
2022-03-23 19:34:50 +0900

    ✨ Product 상세 페이지 구현

---
+++ src/pages/products/[id].tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import ProductDetail from "../../compnents/products/detail";
+import { fetcher, QueryKeys } from "../../queryClient";
+import { Product } from "../../types";
+
+const ProductDetailPage = () => {
+  const { id } = useParams();
+  const { data } = useQuery<Product>(QueryKeys.PRODUCTS, () =>
+    fetcher({
+      method: "GET",
+      path: `/products/${id}`,
+    })
+  );
+  if (!data) return null;
+  return (
+    <div>
+      <h2>상품 상세</h2>
+      <ProductDetail item={data} />;
+    </div>
+  );
+};
+
+export default ProductDetailPage;
