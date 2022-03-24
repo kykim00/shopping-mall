107e42b36ac8fb647e1b3125522cfdc9845fe5be
kykim00 <jeongsaeki@likelion.org>
2022-03-24 18:00:39 +0900

    ♻️ optimistic update 적용하여 api 요청 횟수 줄임

--- src/compnents/cart/item.tsx
+++ src/compnents/cart/item.tsx
@@ -7,16 +7,38 @@ const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
   const queryClient = getClient();
   const { mutate: updateCart } = useMutation(
     ({ id, amount }: { id: string; amount: number }) =>
-      graphqlFetcher(UPDATE_CART, { id, amount })
+      graphqlFetcher(UPDATE_CART, { id, amount }),
+    {
+      onMutate: async ({ id, amount }) => {
+        await queryClient.cancelQueries(QueryKeys.CART);
+        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(
+          QueryKeys.CART
+        );
+        if (!prevCart?.[id]) return prevCart;
+
+        const newCart = {
+          ...(prevCart || {}),
+          [id]: { ...prevCart[id], amount },
+        };
+        queryClient.setQueryData(QueryKeys.CART, newCart);
+        return prevCart;
+      },
+
+      onSuccess: (newValue) => {
+        const prevCart = queryClient.getQueryData<{
+          [key: string]: CartType;
+        }>(QueryKeys.CART);
+        const newCart = {
+          ...(prevCart || {}),
+          ...newValue,
+        };
+        queryClient.setQueryData(QueryKeys.CART, newCart);
+      },
+    }
   );
   const handleUpdateAmount = (e: SyntheticEvent) => {
     const amount = Number((e.target as HTMLInputElement)?.value);
-    updateCart(
-      { id, amount },
-      {
-        onSuccess: () => queryClient.invalidateQueries(QueryKeys.CART),
-      }
-    );
+    updateCart({ id, amount });
   };
   return (
     <li className="cart-item">
