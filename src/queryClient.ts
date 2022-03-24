9230c1e066c328797affa068b70bf2c55f4f0143
kykim00 <jeongsaeki@likelion.org>
2022-03-24 16:24:07 +0900

    ✨ products graphql 구현

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,3 +1,4 @@
+import request, { RequestDocument } from "graphql-request";
 import { QueryClient } from "react-query";
 // import { getTodos, postTodo } from "../my-api";/
 
@@ -21,7 +22,7 @@ export const getClient = (() => {
   };
 })();
 
-const BASE_URL = "https://fakestoreapi.com";
+const BASE_URL = "/";
 export const fetcher = async ({
   method,
   path,
@@ -54,6 +55,10 @@ export const fetcher = async ({
     console.error(err);
   }
 };
+
+export const graphqlFetcher = (query: RequestDocument, variables = {}) =>
+  request(BASE_URL, query, variables);
+
 export const QueryKeys = {
   PRODUCTS: "PRODUCTS",
 };
