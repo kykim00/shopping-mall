9487ad395518e1655f01f5eee357c1dfc7fda9dd
kykim00 <jeongsaeki@likelion.org>
2022-03-26 21:13:38 +0900

    ✨ modal 컴포넌트 구현

---
+++ src/compnents/payment/modal.tsx
@@ -0,0 +1,27 @@
+import { ReactChild } from "react";
+import { createPortal } from "react-dom";
+
+const ModalPortal = ({ children }: { children: ReactChild }) =>
+  createPortal(children, document.getElementById("modal")!);
+
+export const PaymentModal = ({
+  show,
+  proceed,
+  cancel,
+}: {
+  show: boolean;
+  proceed: () => void;
+  cancel: () => void;
+}) => {
+  return show ? (
+    <ModalPortal>
+      <div>
+        <p>정말 결제할까요?</p>
+        <div>
+          <button onClick={proceed}>예</button>
+          <button onClick={cancel}>아니오</button>
+        </div>
+      </div>
+    </ModalPortal>
+  ) : null;
+};
