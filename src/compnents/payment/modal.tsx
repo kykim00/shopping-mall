fd56af9a9e5cd18772f73aaef735a4e004bee3c8
kykim00 <jeongsaeki@likelion.org>
2022-03-28 17:16:47 +0900

    💄 결제 모달 스타일링

--- src/compnents/payment/modal.tsx
+++ src/compnents/payment/modal.tsx
@@ -15,11 +15,13 @@ export const PaymentModal = ({
 }) => {
   return show ? (
     <ModalPortal>
-      <div>
-        <p>정말 결제할까요?</p>
-        <div>
-          <button onClick={proceed}>예</button>
-          <button onClick={cancel}>아니오</button>
+      <div className={`modal ${show ? "show" : ""}`}>
+        <div className="modal__inner">
+          <p>정말 결제할까요?</p>
+          <div>
+            <button onClick={proceed}>예</button>
+            <button onClick={cancel}>아니오</button>
+          </div>
         </div>
       </div>
     </ModalPortal>
