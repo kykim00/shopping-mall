8dc15fbe12932663075a0852ece1648cb96e54c7
kykim00 <jeongsaeki@likelion.org>
2022-04-08 23:12:45 +0900

    ✨ apollo graphql server 구현

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import schema from "./schema";
+import resolvers from "./resolvers";
+
+(async () => {
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+  });
+
+  const app = express();
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/graphql",
+    cors: {
+      origin: ["http://localhost:3000"],
+      credentials: true,
+    },
+  });
+  await app.listen({ port: 8000 });
+  console.log("server listening on port 8000");
+})();
