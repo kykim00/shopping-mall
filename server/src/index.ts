acd629fe4462ad9f9c60c7dd989e08d891b8d3c0
kykim00 <jeongsaeki@likelion.org>
2022-04-09 23:18:03 +0900

    ✨ apolloserver 연결

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,23 +2,30 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import schema from "./schema";
 import resolvers from "./resolvers";
+import { DBfield, readDB } from "./dbController";
 
 (async () => {
   const server = new ApolloServer({
     typeDefs: schema,
     resolvers,
+    context: {
+      db: {
+        products: readDB(DBfield.PRODUDCTS),
+        cart: readDB(DBfield.CART),
+      },
+    },
   });
-
   const app = express();
   await server.start();
   server.applyMiddleware({
     app,
     path: "/graphql",
     cors: {
-      origin: ["http://localhost:3000"],
+      origin: ["http://localhost:3000", "https://studio.apollographql.com"],
       credentials: true,
     },
   });
   await app.listen({ port: 8000 });
+
   console.log("server listening on port 8000");
 })();
