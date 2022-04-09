35f06102867fa42566c7193fc6ff275f2cc07400
kykim00 <jeongsaeki@likelion.org>
2022-04-09 23:20:57 +0900

    ✨ db controller 구현

---
+++ server/src/dbController.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import { resolve } from "path";
+
+export enum DBfield {
+  CART = "cart",
+  PRODUDCTS = "products",
+}
+
+const basePath = resolve();
+
+const filenames = {
+  [DBfield.CART]: resolve(basePath, "src/db/cart.json"),
+  [DBfield.PRODUDCTS]: resolve(basePath, "src/db/products.json"),
+};
+
+export const readDB = (target: DBfield) => {
+  try {
+    return JSON.parse(fs.readFileSync(filenames[target], "utf8"));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const writeDB = (target: DBfield, data: any) => {
+  try {
+    fs.writeFileSync(filenames[target], JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
