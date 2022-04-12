d69e9d259e4d4cebc790f7a72294068052a24dff
kykim00 <jeongsaeki@likelion.org>
2022-04-12 23:59:34 +0900

    🔥 msw 코드 제거

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, useRoutes } from "react-router-dom";
 import "./scss/index.scss";
 import App from "./App";
-import { worker } from "./mocks/browser";
 import { RecoilRoot } from "recoil";
-
-if (import.meta.env.DEV) {
-  worker.start();
-}
 
 ReactDOM.render(
   <React.StrictMode>
