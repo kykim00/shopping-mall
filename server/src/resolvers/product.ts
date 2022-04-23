0a8b394ff31cf21752132bd2d17b89c2c1ad1226
kykim00 <jeongsaeki@likelion.org>
2022-04-23 23:22:32 +0900

    ✨ products mutation 구현

--- server/src/resolvers/product.ts
+++ server/src/resolvers/product.ts
@@ -1,4 +1,8 @@
-import { Resolver } from "./types";
+import { Products, Resolver } from "./types";
+import { v4 as uuid } from "uuid";
+import { DBfield, writeDB } from "../dbController";
+
+const setJSON = (data: Products) => writeDB(DBfield.PRODUDCTS, data);
 
 const productResolver: Resolver = {
   Query: {
@@ -15,6 +19,44 @@ const productResolver: Resolver = {
       const found = db.products.find((item) => item.id === id);
       if (found) return found;
       return null;
+    },
+  },
+  Mutation: {
+    addProduct: (parent, { imageUrl, price, title, description }, { db }) => {
+      const newProduct = {
+        id: uuid(),
+        imageUrl,
+        price,
+        title,
+        description,
+        createdAt: Date.now(),
+      };
+      db.products.push(newProduct);
+      setJSON(db.products);
+      return newProduct;
+    },
+    updateProduct: (parent, { id, ...data }, { db }) => {
+      const existProductIndex = db.products.findIndex((item) => item.id === id);
+      if (existProductIndex < 0) {
+        throw new Error("없는 상품입니다.");
+      }
+      const updatedItem = { ...db.products[existProductIndex], ...data };
+      db.products.splice(existProductIndex, 1, updatedItem);
+      setJSON(db.products);
+      return updatedItem;
+    },
+    deleteProduct: (parent, { id }, { db }) => {
+      const existProductIndex = db.products.findIndex((item) => item.id === id);
+      if (existProductIndex < 0) {
+        throw new Error("없는 상품입니다.");
+      }
+      const updatedItem = {
+        ...db.products[existProductIndex],
+      };
+      delete updatedItem.createdAt;
+      db.products.splice(existProductIndex, 1, updatedItem);
+      setJSON(db.products);
+      return id;
     },
   },
 };
